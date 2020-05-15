7495314786fb883eefe3180455e38aeb5cd44b6f
bitranox <rnowotny1966@gmail.com>
2020-05-16 01:08:11 +0200

    refractor

--- lib_programname/__init__.py
+++ lib_programname/__init__.py
@@ -5,10 +5,10 @@ from main import *
 
 def get_version() -> str:
     with open(str(pathlib.Path(__file__).parent / 'version.txt'), mode='r') as version_file:
-        version = version_file.readline()
+        version = version_file.readline().strip()
     return version
 
 
-__title__ = pathlib.Path(__file__).parts[-1]
+__title__ = pathlib.Path(__file__).parts[-2]
 __version__ = get_version()
 __name__ = __title__
