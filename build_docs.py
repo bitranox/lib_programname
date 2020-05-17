3975691a930f9e8f710ad0b90c998f69be9298d1
bitranox <rnowotny1966@gmail.com>
2020-05-17 13:29:42 +0200

    refractor

--- build_docs.py
+++ build_docs.py
@@ -57,29 +57,11 @@ def main(args: Dict[str, str]) -> None:
                         target='./README.rst')
 
     logger.info('replace repository related strings')
-    rst_include.rst_str_replace(source='./README.rst',
-                                target='',
-                                old='{repository_slug}',
-                                new=travis_repo_slug,
-                                inplace=True)
-    rst_include.rst_str_replace(source='./README.rst',
-                                target='',
-                                old='{repository}',
-                                new=repository,
-                                inplace=True)
-    rst_include.rst_str_replace(source='./README.rst',
-                                target='',
-                                old='{repository_dashed}',
-                                new=repository_dashed,
-                                inplace=True)
+    rst_include.rst_str_replace(source='./README.rst', target='', old='{repository_slug}', new=travis_repo_slug, inplace=True)
+    rst_include.rst_str_replace(source='./README.rst', target='', old='{repository}', new=repository, inplace=True)
+    rst_include.rst_str_replace(source='./README.rst', target='', old='{repository_dashed}', new=repository_dashed, inplace=True)
     rst_include.rst_str_replace(source='./README.rst', target='', old='{last_update_yyyy}', new=str(datetime.date.today().year + 1), inplace=True)
-
-    rst_include.rst_str_replace(source='./README.rst',
-                                target='',
-                                old='{codeclimate_link_hash}',
-                                new=codeclimate_link_hash,
-                                inplace=True)
-
+    rst_include.rst_str_replace(source='./README.rst', target='', old='{codeclimate_link_hash}', new=codeclimate_link_hash, inplace=True)
     logger.info('done')
     sys.exit(0)
 
