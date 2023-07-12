10af1cf75b967d88703895e752af238fb56a1257
bitranox <rnowotny1966@gmail.com>
2023-07-12 23:48:41 +0200

    clean ./tests/test_cli.py

--- tests/script_without_extension
+++
@@ -1,6 +0,0 @@
-#!/opt/Python-3.11.4/bin/python3.11
-import pathlib
-import lib_programname
-# this returns the fully resolved path to the launched python program
-path_to_program = lib_programname.get_path_executed_script()  # type: pathlib.Path
-print(path_to_program)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,9 +8,11 @@ logger = logging.getLogger()
 package_dir = "lib_programname"
 cli_filename = "lib_programname_cli.py"
 
+path_cli_command = pathlib.Path(__file__).resolve().parent.parent / package_dir / cli_filename
 
-def call_cli_command(cli_command: str, commandline_args: str) -> bool:
-    command = " ".join([cli_command, commandline_args]).strip()
+
+def call_cli_command(commandline_args: str = '') -> bool:
+    command = ' '.join([sys.executable, str(path_cli_command), commandline_args])
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError:
@@ -19,54 +21,8 @@ def call_cli_command(cli_command: str, commandline_args: str) -> bool:
 
 
 def test_cli_commands() -> None:
-    cli_command = " ".join([sys.executable, str(pathlib.Path(__file__).resolve().parent.parent / package_dir / cli_filename)]).strip()
-    assert not call_cli_command(cli_command, "--unknown_option")
-    assert call_cli_command(cli_command, "--version")
-    assert call_cli_command(cli_command, "-h")
-    assert call_cli_command(cli_command, "info")
-    assert call_cli_command(cli_command, "--traceback info")
-
-
-def execute_script(script_name: str) -> str:
-    """
-    >>> # shebang without extension
-    >>> ignore = set_shebang("script_without_extension")
-    >>> execute_script("script_without_extension")
-    '/.../lib_programname/tests/script_without_extension'
-
-    >>> # shebang with extension
-    >>> ignore = set_shebang("script_with_extension.py")
-    >>> execute_script("script_with_extension.py")
-    '/.../lib_programname/tests/script_with_extension.py'
-
-    """
-    # cli_command = " ".join([sys.executable, get_str_path_to_script(script_name)]).strip()
-    cli_command = get_str_path_to_script(script_name)
-    result = subprocess.run(cli_command, shell=True, check=True, capture_output=True).stdout.decode('utf-8').strip()
-    return result
-
-
-def get_str_path_to_script(script_file_name: str) -> str:
-    """
-    >>> get_str_path_to_script('test_script')
-    '/.../lib_programname/tests/test_script'
-
-    """
-    path_to_script = get_test_directory() / script_file_name
-    str_path_to_script = str(path_to_script).strip()
-    return str_path_to_script
-
-
-def set_shebang(script_file_name: str) -> str:
-    """
-    set shebang of the criptfile to the current python interpreter
-    >>> assert set_shebang("script_with_extension.py").startswith(f'#!{sys.executable}')
-    """
-    my_txt = (get_test_directory() / script_file_name).read_text()
-    my_txt = my_txt.replace('/usr/bin/python3', str(sys.executable))
-    (get_test_directory() / script_file_name).write_text(my_txt)
-    return my_txt
-
-
-def get_test_directory() -> pathlib.Path:
-    return pathlib.Path(__file__).resolve().parent
+    assert not call_cli_command('--unknown_option')
+    assert call_cli_command('--version')
+    assert call_cli_command('-h')
+    assert call_cli_command('info')
+    assert call_cli_command('--traceback info')
