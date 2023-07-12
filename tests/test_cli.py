9d0cfa5ea1ab878b9249ad3437fba9d7caa78a1d
bitranox <rnowotny1966@gmail.com>
2023-07-12 22:22:32 +0200

    clean ./tests/test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,11 +8,9 @@ logger = logging.getLogger()
 package_dir = "lib_programname"
 cli_filename = "lib_programname_cli.py"
 
-path_cli_command = pathlib.Path(__file__).resolve().parent.parent / package_dir / cli_filename
 
-
-def call_cli_command(commandline_args: str = "") -> bool:
-    command = " ".join([sys.executable, str(path_cli_command), commandline_args])
+def call_cli_command(cli_command: str, commandline_args: str) -> bool:
+    command = " ".join([cli_command, commandline_args]).strip()
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError:
@@ -21,8 +19,40 @@ def call_cli_command(commandline_args: str = "") -> bool:
 
 
 def test_cli_commands() -> None:
-    assert not call_cli_command("--unknown_option")
-    assert call_cli_command("--version")
-    assert call_cli_command("-h")
-    assert call_cli_command("info")
-    assert call_cli_command("--traceback info")
+    cli_command = " ".join([sys.executable, str(pathlib.Path(__file__).resolve().parent.parent / package_dir / cli_filename)]).strip()
+    assert not call_cli_command(cli_command, "--unknown_option")
+    assert call_cli_command(cli_command, "--version")
+    assert call_cli_command(cli_command, "-h")
+    assert call_cli_command(cli_command, "info")
+    assert call_cli_command(cli_command, "--traceback info")
+
+
+def test_execute_script_with_shebang(script_name: str) -> str:
+    """
+    >>> # shebang without extension
+    >>> test_execute_script_with_shebang("test_shebang")
+    '/.../lib_programname/tests/test_shebang'
+
+    >>> test_execute_script_with_shebang("test_shebang.py")
+    '/.../lib_programname/tests/test_shebang.py'
+
+    """
+    # cli_command = " ".join([sys.executable, str(pathlib.Path(__file__).resolve().parent / "test_shebang")]).strip()
+    cli_command = get_str_path_to_script(script_name)
+    result = subprocess.run(cli_command, shell=True, check=True, capture_output=True).stdout.decode('utf-8').strip()
+    return result
+
+
+def get_str_path_to_script(script_name: str) -> str:
+    """
+    >>> get_str_path_to_script('test_script')
+    '/.../lib_programname/tests/test_script'
+
+    """
+    path_to_script = get_test_directory() / script_name
+    str_path_to_script = str(path_to_script).strip()
+    return str_path_to_script
+
+
+def get_test_directory() -> pathlib.Path:
+    return pathlib.Path(__file__).resolve().parent
---
+++ tests/test_shebang.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import pathlib
+import lib_programname
+# this returns the fully resolved path to the launched python program
+path_to_program = lib_programname.get_path_executed_script()  # type: pathlib.Path
+print(path_to_program)
